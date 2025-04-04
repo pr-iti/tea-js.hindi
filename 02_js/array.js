0063107fed2fc1309d41360d944e2a3242b605d3
pr-iti <priti90066@gmail.com>
2025-04-04 18:07:59 +0000

    arrays in js

---
+++ 02_js/array.js
@@ -0,0 +1,29 @@
+//Array
+const myarr=[0,1,2,3,2,5];
+const heroes=["priti","rani","nikhil"];
+
+// console.log(myarr[1]);
+
+const arr2=myarr;
+const arr3=new Array(0,3,4,5);//another way to declare array
+// console.log(arr3[3]);
+
+arr2.push(6);// to add values
+
+// arr2.push(3);
+// console.log(arr2);
+ 
+// arr2.pop();//delete the last element
+// console.log(arr2);
+
+// arr2.unshift(3);//add the values  given in the parenthesis at the start of the array
+
+// console.log(arr2);
+// arr2.shift();//removes the added value;
+
+console.log(arr2.includes(3));//3 is present or not...result in bool
+console.log(arr2.indexof(3));
+
+ console.log(arr2);
+
+
